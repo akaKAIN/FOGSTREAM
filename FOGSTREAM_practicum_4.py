ed2995797e77f1323d2b6eddafaedbcc98999875
akaKAIN <mkain@mail.ru>
2019-04-07 17:11:01 +1000

    Первое задание выполнено.
    - все требуемые операторы перегружены

--- FOGSTREAM_practicum_4.py
+++ FOGSTREAM_practicum_4.py
@@ -22,41 +22,90 @@
 # если это возможно. Например дробь 12/16 должны быть преобразована к 3/4.
 import math
 
+
 # Создаем класс косой дроби (со встроенным сокращением)
 class Fraction:
-    def __init__(self, num, denom):
-        reduct_num = math.gcd(num, denom)
-        self.num = num // reduct_num
-        self.denom = denom // reduct_num
-
-    # Функция сокращения дроби.
-    def reduction(self):
-        reduct_num = math.gcd(self.num, self.denom)
-        return (self.num // reduct_num, self.denom // reduct_num)
+    def __init__(self, a, b):
+        reduct_a = math.gcd(a, b)
+        self.a = a // reduct_a
+        self.b = b // reduct_a
 
     # Перегрузка оператора сырой строки
     def __repr__(self):
-        return '(Дробь {}/{})'.format(self.num, self.denom)
+        return '(Дробь {}/{})'.format(self.a, self.b)
 
     # Перегрузка оператора вывода print
     def __str__(self):
-        return '{}/{}'.format(self.num, self.denom)
+        return '{}/{}'.format(self.a, self.b)
 
     # Перегрузка оператора сложения
     def __add__(self, other):
-        return Fraction((self.num * other.denom) + (self.denom * other.num),
-                        self.denom * other.denom)
+        return Fraction((self.a * other.b) + (self.b * other.a),
+                        self.b * other.b)
 
     # Перегрузка оператора вычитания
     def __sub__(self, other):
-        return Fraction((self.num * other.denom) - (self.denom * other.num),
-                        self.denom * other.denom)
+        return Fraction((self.a * other.b) - (self.b * other.a),
+                        self.b * other.b)
+
+    # Перегрузка оператора <
+    def __lt__(self, other):
+        if (self.a * other.b) < (self.b * other.a):
+            return True
+        elif (self.a * other.b) > (self.b * other.a):
+            return False
+        elif (self.a * other.b) == (self.b * other.a):
+            return False
+
+    # Перегрузка оператора <=
+    def __le__(self, other):
+        if (self.a * other.b) < (self.b * other.a):
+            return True
+        elif (self.a * other.b) > (self.b * other.a):
+            return False
+        elif (self.a * other.b) == (self.b * other.a):
+            return True
+
+    # Перегрузка оператора >
+    def __gt__(self, other):
+        if (self.a * other.b) > (self.b * other.a):
+            return True
+        elif (self.a * other.b) < (self.b * other.a):
+            return False
+        elif (self.a * other.b) == (self.b * other.a):
+            return False
+
+    # Перегрузка оператора >=
+    def __ge__(self, other):
+        if (self.a * other.b) > (self.b * other.a):
+            return True
+        elif (self.a * other.b) < (self.b * other.a):
+            return False
+        elif (self.a * other.b) == (self.b * other.a):
+            return True
+
+    # Перегрезка оператора умножения.
+    def __mul__(self, other):
+        return Fraction((self.a * other.a), (self.b * other.b))
+
+    # Перегрезка оператора деления
+    def __truediv__(self, other):
+        return Fraction((self.a * other.b), (self.b * other.a))
 
 
 
-
-frac1 = Fraction(4, 2)
+frac1 = Fraction(1, 5)
 # frac1 = fraction_reduction(frac1)
-frac2 = Fraction(1, 3)
+frac2 = Fraction(1, 5)
 # frac2 = fraction_reduction(frac2)
 print(f'{frac1} + {frac2} = {frac1 + frac2}')
+
+print(f'{frac1} - {frac2} = {frac1 - frac2}')
+
+print(f'{frac1} > {frac2} = {frac1 > frac2}')
+
+print(f'{frac1} < {frac2} = {frac1 < frac2}')
+
+print(f'{frac1} * {frac2} = {frac1 * frac2}')
+
+print(f'{frac1} / {frac2} = {frac1 / frac2}')
