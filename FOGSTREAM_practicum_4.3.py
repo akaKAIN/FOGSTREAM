30c74f54353bab90e6ca041d26818991ff5d1f1d
akaKAIN <mkain@mail.ru>
2019-04-09 12:43:06 +1000

    Выполнил задание №3

--- FOGSTREAM_practicum_4.3.py
+++ FOGSTREAM_practicum_4.3.py
@@ -11,23 +11,62 @@
 # коде вызовом встроенной функции str(c).
 #
 #  Реализовать операции с компексными числами:
-# - сложение
-# - вычитание
-# - умножение
-# - деление
-# - ВЫВОД (функция str(c))
+# - сложение +
+# - вычитание +
+# - умножение +
+# - деление +
+# - ВЫВОД (функция str(c)) +
 
 class Complex:
     def __init__(self, real, imag):
-        self.real = int(real)
-        self.imag = int(imag)
+        self.real = real
+        self.imag = imag
 
     def __str__(self):
-        return str(complex(self.real, self.imag))
+        sign = ''
+        if self.imag > 0:
+            sign = '+'
+        return '({:.2f}{}{:.2f}j)'.format(
+            round(self.real, 2), sign, round(self.imag, 2)
+        )
 
     def __add__(self, other):
-        return 
+        return Complex(self.real + other.real, self.imag + other.imag)
+
+    def __sub__(self, other):
+        return Complex(self.real - other.real, self.imag - other.imag)
+
+    def __mul__(self, other):
+        x1, y1, x2, y2 = splitting_into_variables(self, other)
+        real = (x1 * x2 - y1 * y2)
+        imag = (x1 * y2 + x2 * y1)
+        return Complex(real, imag)
+
+    def __truediv__(self, other):
+        x1, y1, x2, y2 = splitting_into_variables(self, other)
+        real = (x1 * x2 + y1 * y2) / (x2 ** 2 + y2 ** 2)
+        imag = (x2 * y1 - x1 * y2) / (x2 ** 2 + y2 ** 2)
+        return Complex(real, imag)
 
 
-x = Complex(1,2)
-print(x)
+def splitting_into_variables(self, other):
+    x1 = self.real
+    y1 = self.imag
+    x2 = other.real
+    y2 = other.imag
+    return x1, y1, x2, y2
+
+
+
+z1 = Complex(10, 2)
+z2 = Complex(8, 32)
+print(f"""
+z1 = {z1}
+z2 = {z2}""")
+print(f'z1 + z2 = {z1 + z2}')
+print(f'z1 - z2 = {z1 - z2}')
+print(f'z1 * z2 = {z1 * z2}')
+print(f'z1 / z2 = {z1 / z2}')
+print(f"""
+z1 = {z1}
+z2 = {z2}""")
