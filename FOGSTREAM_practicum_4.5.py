3e766e30b6b840dcfa6ffb16155fe5e3061ce32b
akaKAIN <mkain@mail.ru>
2019-04-09 22:45:16 +1000

    Выполнил задание №5

--- FOGSTREAM_practicum_4.5.py
+++ FOGSTREAM_practicum_4.5.py
@@ -4,10 +4,23 @@
 # Метод distance должен выбрасывать исключение  ValueError, если в него передан параметр неверного типа.
 
 class Point3D:
-    def __init__(self, x, y, z):
+    def __init__(self, x=0, y=0, z=0):
         self.x = x
         self.y = y
         self.z = z
 
-    def dictance(self, other):
-        
+#     def distance(self, other):
+#         try:
+#             raise ErrorCatcher('Ввот не тод')
+#         except ErrorCatcher as e:
+#             print(e.args)
+#         # return ((other.x - self.x) ** 2 + (other.y - self.y) ** 2 + (other.z - self.z) ** 2) ** 0.5
+#
+# class ErrorCatcher(ValueError):
+#     def __init__(self, arg):
+#         self.arg = arg
+
+
+point1 = Point3D(1, 2, 3)
+
+print(type(point1) == Point3D)
