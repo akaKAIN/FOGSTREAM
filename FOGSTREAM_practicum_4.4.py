abfd0b1724e57f32af1a2d7275d93104f200cb32
akaKAIN <mkain@mail.ru>
2019-04-09 13:20:57 +1000

    Попытка создаия классов

--- FOGSTREAM_practicum_4.4.py
+++ FOGSTREAM_practicum_4.4.py
@@ -4,18 +4,30 @@
 # Конструктор класса Rectangle принимает 2 параметра - длину и ширину;
 # класса Circle - один параметр - радиус; класса Triangle - три параметра - стороны треугольника.
 #
-class Figure:
-    def area(self):
-        pass
+import math
 
-    def perimeter(self):
-        pass
+class Figure:
+    def area_circle(self, r):
+        return math.pi * r ** 2
+
+    def perimeter_circle(self, r):
+        return 2 * math.pi * r
+
+
 
 class Triangle(Figure):
-    pass
+    def __init__(self, side_a, side_b, side_c):
+        self.side_a = side_a
+        self.side_b = side_b
+        self.side_c = side_c
+
 
 class Rectangle(Figure):
-    pass
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
 
 class Circle(Figure):
-    pass
+    def __init__(self, radius):
+        self.radius = radius
